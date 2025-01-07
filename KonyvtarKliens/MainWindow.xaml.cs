79c4fb937d006bd28fb22c34f43feaecdc40c0ce
ZZalan001 <zima.zalan1@gmail.com>
2025-01-07 14:39:23 +0100

    Add project files.

---
+++ KonyvtarKliens/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using KonyvtarApi.Models;
+using KonyvtarKliens.DTOs;
+using KonyvtarKliens.Services;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KonyvtarKliens
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static HttpClient sharedClient = new() 
+        {
+            BaseAddress = new("http://localhost:5000") 
+        };
+        private static List<KonyvtarakDTO> konyvtarak = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            FeltoltKonyvtar();
+        }
+
+        private void btnUj_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnModosit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTorol_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void FeltoltKonyvtar()
+        {
+            konyvtarak = await KonyvtarService.GetAll(sharedClient);
+            Task.Delay(1000).Wait();
+            dgrKonyvtarak.ItemsSource = konyvtarak;
+        }
+    }
+}
---
+++ KonyvtarKliens/Services/KonyvtarService.cs
@@ -0,0 +1,20 @@
+﻿using KonyvtarApi.Models;
+using KonyvtarKliens.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonyvtarKliens.Services
+{
+    internal class KonyvtarService
+    {
+        public static async Task<List<KonyvtarakDTO>> GetAll(HttpClient client)
+        {
+            return await client.GetFromJsonAsync<List<KonyvtarakDTO>>("Konyvtar/GetAll");
+        }
+    }
+}
